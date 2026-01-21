2f8203ccc035ef87c3948ddc4071fb449bb49b08
pb-06 <kkandomail@gmail.com>
2026-01-21 09:04:59 +0100

    Apply POST endpoint

--- src/index.cjs
+++ src/index.cjs
@@ -5,10 +5,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+var addresses = [];
+
 // endpoints
 app.get('/hello', (req, res) => {
     return res.status(200).json({ messages: "Hello!" });
 });
+
+app.post('/geolocation', (req, res) => {
+    const { latitude, longitude } = req.body
+    console.log({ latitude, longitude })
+    addresses.push({ latitude, longitude })
+
+    return res.status(201).json('ok')
+})
 
 const port = 3333;
 var actualPort = process.env.PORT || port;
